676d4e439dfdc75824a27df5a7bc77c968911e38
Rexios <rexios@rexios.dev>
2023-02-19 12:27:51 -0500

    Initial commit

---
+++ bin/parselyzer.dart
@@ -0,0 +1,5 @@
+import 'package:parselyzer/parselyzer.dart' as parselyzer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${parselyzer.calculate()}!');
+}
---
+++ test/parselyzer_test.dart
@@ -0,0 +1,8 @@
+import 'package:parselyzer/parselyzer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
